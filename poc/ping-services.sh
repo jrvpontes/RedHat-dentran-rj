c6223e923186bd87ddb88f08a38a819720c8b42a
João Pontes <jrvpontes@gmail.com>
2022-03-29 02:32:33 -0300

    AS-Build
    https://docs.google.com/document/d/1_Plm43jmhRK74AAdP7_R6CHtjj8ef4g6d_XzqxA4daM/edit
    
    https://docs.google.com/document/d/1Fqf_jkZ1XjVzNsaNC27u_REpxO6ZHuX5HhZVQ2FhmZw/edit#heading=h.gjdgxs

--- poc/ping-services.sh
+++ poc/ping-services.sh
@@ -1,5 +1,6 @@
 #curl localhost:8080/bv
 #curl localhost:8888/order
-curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/order
-curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/spl50
-curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/rating
+HOST=http://istio-ingressgateway-detran-rj-infra-istio-v13.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com
+curl $HOST/order
+curl $HOST/spl50
+curl $HOST/rating
