a70c18d3bcc7db3f004511ed83113df7b8449425
João Pontes <jrvpontes@gmail.com>
2022-03-28 23:46:06 -0300

    service
    adição de um serviço

--- poc/ping-services.sh
+++ poc/ping-services.sh
@@ -2,3 +2,4 @@
 #curl localhost:8888/order
 curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/order
 curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/spl50
+curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/rating
