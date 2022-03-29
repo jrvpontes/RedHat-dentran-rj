6adcaf50030d080aacd3eaf24fbc305c905d77f0
João Pontes <jrvpontes@gmail.com>
2022-03-28 21:48:13 -0300

    v4 - primeira instalação
    primeiro versão de roteiro bem sucedida.

--- poc/ping-services.sh
+++ poc/ping-services.sh
@@ -1,2 +1,4 @@
-curl localhost:8080/bv
-curl localhost:8888/order
+#curl localhost:8080/bv
+#curl localhost:8888/order
+curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/order
+curl http://istio-ingressgateway-detran-rj-infra-istio-v4.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com/spl50
